c3ab96441b90e78ee73604073a0fd2f85cd12838
Mohanavenkatesh <mohanavenkatesh.a@gmail.com>
2025-09-16 07:18:48 +0530

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,49 +1,12 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-
-import Men from '../assets/banner/men-banner.avif'
-import Kids from '../assets/banner/kids-banner.avif'
-import Women from '../assets/banner/women-banner.avif'
-
+import desktop_banner from '../assets/banner/banner-desktop-by-mohan.avif';
 
 export default function Hero() {
     return (
-        <div className="w-full ">
-            <Swiper
-                spaceBetween={30}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
-                // navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <SwiperSlide>
-                    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 rounded-lg">
-                        <img className='' src={Men} alt="menimg" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 rounded-lg">
-                        <img className='' src={Kids} alt="menimg" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 rounded-lg">
-                        <img className='' src={Women} alt="menimg" />
-                    </div>
-                </SwiperSlide>
+        <div className="flex justify-center items-center w-full">
 
-            </Swiper>
+            <img src={desktop_banner} alt="" />
+
         </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,30 @@ import { ShoppingCart } from 'lucide-react';
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between font- items-center mx-auto mt-5 gap-5 p-5 border border-b-stone-700 rounded-xl max-w-[1400px]' >
-      <div>
-        <h1>React Store</h1>
-      </div>
-      <div className='flex gap-5'>
-        <Link to='/'>Home</Link>
+    <div className='flex justify-between mx-auto gap-5 p-5 ' >
+      <div className='flex justify-start font- items-center'>
+        <div>
+          <h1>LOGO</h1>
+        </div>
+        <div className='flex gap-5 ms-5'>
+          {/* <Link to='/'>Home</Link>
         <Link to='/About'>About</Link>
         <Link to='/Products'>Products</Link>
-        <Link to='/Content'>Contact</Link>
+        <Link to='/Content'>Contact</Link> */}
+          <Link to='/'>HOME</Link>
+          <Link to='/Content'>SHOP</Link>
+          <Link to='/Content'>BEST SELLERS</Link>
+          <Link to='/Content'>NEW ARRIVALS</Link>
+          <Link to='/Content'>EARRINGS</Link>
+          <Link to='/Content'>NECKLACES</Link>
+          <Link to='/Content'>RINGS</Link>
+        </div>
       </div>
-      <div>
-        <Link to='Cart'><ShoppingCart /></Link>
+
+      <div className='flex justify-end items-center gap-5'>
+        <Link to='/Content'>ACCOUNT</Link>
+        <Link to='/Content'>WHISHLIST</Link>
+        <Link to='/Content'>BAG</Link>
       </div>
     </div>
   )
