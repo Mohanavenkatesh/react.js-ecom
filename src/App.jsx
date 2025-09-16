23f4db7fc43ec1b9145de3606d6aa36687c5c73a
Mohanavenkatesh <mohanavenkatesh.a@gmail.com>
2025-09-16 11:10:19 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
-import { Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
 import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
 import Home from './pages/Home'
-import About from './pages/About'
-import Products from './pages/Products'
-import Contact from './pages/Contact'
-import Faq from './pages/Faq'
-import Error from './pages/Error'
-import Cart from './pages/Cart'
-import Men from './pages/Men'
-import Women from './pages/Women'
-import Kids from './pages/Kids'
 
 function App() {
 
   return (
     <>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} ></Route>
-          <Route path='/About' element={<About />} ></Route>
-          <Route path='/Products' element={<Products />} ></Route>
-          <Route path='/Content' element={<Contact />} ></Route>
-          <Route path='/Faq' element={<Faq />} ></Route>
-          <Route path='/Cart' element={<Cart />} ></Route>
-          <Route path='/Men' element={<Men />} ></Route>
-          <Route path='/Women' element={<Women />} ></Route>
-          <Route path='/Kids' element={<Kids />} ></Route>
-          <Route path='*' element={<Error />} ></Route>
-        </Routes>
-        <Footer />
-      </div>
+      <Home />
+      
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,48 @@
 import React from 'react'
-import Hero from '../components/Hero'
-import Category from '../components/Home-Page/Category'
+// banner
+import bannerimgdesktop from '../assets/home/banner/desktop-banner-by-mohan.avif'
+import bannerimgmobile from '../assets/home/banner/mobile-banner-by-mohan.avif'
+// categories
+import categoryimg1 from '../assets/home/category/bracelets.avif'
+import categoryimg2 from '../assets/home/category/earrings.avif'
+import categoryimg3 from '../assets/home/category/necklaces.avif'
+import categoryimg4 from '../assets/home/category/rings.avif'
+import categoryimg5 from '../assets/home/category/earrings-2.avif'
+import categoryimg6 from '../assets/home/category/rings-2.avif'
+import EmblaCarousel from '../components/Carousel'
+import Carousel from '../components/Carousel'
+
 
 const Home = () => {
-  return (
+    return (
+        <div>
+            <div className="container mx-auto">
+                {/* banner image */}
+                <img className='hidden md:block' src={bannerimgdesktop} alt="bannerimg" />
+                <img className='block md:hidden' src={bannerimgmobile} alt="bannerimg" />
+                {/* categories */}
+                <div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 my-4">
+                        <img src={categoryimg1} alt="bracelets" />
+                        <img src={categoryimg2} alt="earrings" />
+                        <img src={categoryimg3} alt="necklaces" />
+                        <img src={categoryimg4} alt="rings" />
+                    </div>
+                </div>
 
-    <div>
-      <Hero />
-      <Category />
-    </div>
-  )
+                <div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 my-4">
+                        <img src={categoryimg5} alt="bracelets" />
+                        <img src={categoryimg6} alt="bracelets" />
+                    </div>
+                </div>
+                {/* Best Sellers  */}
+                <div>
+                    {/* <Carousel/> */}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
